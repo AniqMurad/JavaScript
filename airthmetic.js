030f9df0636b163930d32d62908622c3df9d21ec
AniqMurad <nathaaniq0506@gmail.com>
2022-09-12 19:15:00 +0500

    arithmetic operations

---
+++ airthmetic.js
@@ -0,0 +1,66 @@
+var x= 52, y=10;
+x= x%y;
+x
+
+var a = 10, b = 2;
+a= a**b;
+a
+
+var c = 3 === '3';
+c
+
+var d = 3 == 3;
+d
+
+var e = 4 == '4';
+e
+
+var f = 3 != '3';
+f
+
+var g = 3 != '4';
+g
+
+var h = 3 !== '3';
+h
+
+var i = 3 !== 3;
+i
+
+var j = 3 > 3;
+j
+
+var k =3>=3;
+k
+
+var l = 7<5;
+l
+
+var m = 7;
+m++ 
+m
+
+++m
+m
+
+--m
+m
+
+m--
+m
+
+m = -m;
+m
+
+var n = "7";
+n = +n;
+n
+
+var o = true;
+o = +o;
+o
+
+var p = "namaste", q="london";
+p = p+ " " + q;
+p
+
